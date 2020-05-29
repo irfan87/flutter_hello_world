a0055d1733880b6f6f455818fc56f7cbcb3f393a
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-30 01:12:36 +0800

    scaffolded the icons at the material app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hello_flutter/ui/home.dart';
 
 void main() {
-  runApp(Home());
+  // runApp(Home());
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: ScaffoldExample());
+  }
 }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -22,3 +22,36 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+class ScaffoldExample extends StatelessWidget {
+  iconEmailPressed() => debugPrint('Icon Email was tapped');
+
+  iconACPressed() => debugPrint("Icon AC was tapped");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scaffold"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent.shade200,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.ac_unit, color: Colors.white),
+            onPressed: iconACPressed,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.email,
+              color: Colors.white,
+            ),
+            onPressed: iconEmailPressed,
+          )
+        ],
+      ),
+      body: Center(
+        child: Home(),
+      ),
+    );
+  }
+}
